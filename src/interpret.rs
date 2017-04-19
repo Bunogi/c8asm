3f46f75a53cb6b6de10d50df00da8bda978834cc
Bunogi <2bunogi@gmail.com>
2017-04-19 19:27:07 +0200

    Read from file

--- src/interpret.rs
+++ src/interpret.rs
@@ -1,30 +1,45 @@
-use std::string;
 use std::result;
 use std::num;
 
 use instructions::*;
 
 //Returns a string with comments removed, with everything in lowercase
-pub fn sanitize_line(input: &String) -> String {
-	input.clone() //Please implement me
+pub fn sanitize_line(input: &mut String){
+	let index = match input.find(',') {
+		Some(v) => v,
+		None => 0,
+	};
+
+	if index > 0 {
+		if input.remove(index) != ',' { panic!("Removed wrong comma"); }
+	}
+	//TODO: remove comments from input
+	println!("Input: {}", *input);
+	*input = input.to_lowercase();
 }
 
 fn convert_number(input: &str) -> Result<u16, String> {
 	let convert = input.to_string();
 	let result: result::Result<u16, num::ParseIntError>;
-	if convert[..2].to_string() == "0x" {
-		result = convert[2..].parse::<u16>();
-	} else {
-		result = u16::from_str_radix(&convert[..], 16);
+	if convert.chars().count() >= 2 {
+		if convert[..2].to_string() == "0x" {
+			result = u16::from_str_radix(&convert[2..], 16);
+		} else {
+			result = convert.parse::<u16>();
+		}
 	}
+	else {
+		result = convert.parse::<u16>();
+	}
+
 	match result {
 		Ok(num) => return Ok(num),
-		Err(_) => return Err("Not a number: ".to_string() + input),
-	}
+		Err(_)  => return Err("Not a number: ".to_string() + input),
+	};
 }
 
 fn interpret_ld_instruction(data: &Vec<&str>) -> Result<(CPUInstruction, u16), String> {
-	//LD Vx, y
+	//LD Vx, Vy
 	if data[1].chars().nth(0).unwrap() == 'v' {
 		if data[1].chars().count() > 2 {
 			return Err("Unknown register: ".to_string() + data[1]);
@@ -35,23 +50,37 @@ fn interpret_ld_instruction(data: &Vec<&str>) -> Result<(CPUInstruction, u16), S
 				Ok(n) => {
 					let x = convert_number(&data[1][1..]);
 					match x {
-						Ok(m) => return Ok((CPUInstruction::LD_Vy, (m >> 4) | (n >> 8))),
+						Ok(m)    => return Ok((CPUInstruction::LD_Vy, (m << 4) | (n << 8))),
 						Err(msg) => return Err(msg),
 					}
 				}
 				Err(msg) => return Err(msg),
 			}
 		}
+		//LD Vx, *
 		match data[2] {
 			"dt"  => return Ok((CPUInstruction::LD_Vx_DT, 0)),
 			"k"   => return Ok((CPUInstruction::LD_K, 0)),
 			"[i]" => return Ok((CPUInstruction::LD_Vx_ADDR_I, 0)),
-			_     => {
+			_     => { //LD Vx, byte
+				let x: u16;
+				let byte: u16;
+
 				let convert = convert_number(&data[1][1..]);
 				match convert {
-					Ok(n) => return Ok((CPUInstruction::LD, n)),
+					Ok(n)    => {
+						x = n;
+					},
 					Err(msg) => return Err(msg),
 				}
+
+				let convert = convert_number(&data[2]);
+				match convert {
+					Ok(n)    => byte = n & 0x00FF,
+					Err(msg) => return Err(msg),
+				}
+				return Ok((CPUInstruction::LD, (x << 8) | byte));
+
 			}
 		}
 	}
@@ -60,14 +89,16 @@ fn interpret_ld_instruction(data: &Vec<&str>) -> Result<(CPUInstruction, u16), S
 }
 
 //Returns two bytes to be written to the binary file
-pub fn interpret_line(input: &String) -> Result<u16, String> {
-	let input = sanitize_line(input);
+pub fn interpret_line(input: &mut String) -> Result<u16, String> {
+	sanitize_line(input);
 	let data: Vec<&str> = input.split(' ').collect();
 
-	//let (instruction, operand) = match data[0] {
-	//	"ld" => get_ld_variant(&data),
-	//	_ => (CPUInstruction::empty, 0),
-	//};
+	match data[0] {
+		"ld" => match interpret_ld_instruction(&data) {
+			Ok((ins, op)) => return Ok(convert_to_opcode(ins, op)),
+			Err(s)        => return Err(s),
+		},
+		_    => return Err("Failed to parse line: {}".to_string()  + input),
+	};
 
-	Ok(convert_to_opcode(CPUInstruction::DRW, 0xFFFF))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 extern crate clap;
 
-use clap::{Arg, App, SubCommand};
+use clap::{Arg, App};
 
 use std::fs::File;
+use std::io::{BufReader, BufRead};
+use std::process;
 
 mod instructions;
 use instructions::*;
@@ -11,18 +13,31 @@ mod interpret;
 use interpret::*;
 
 fn main() {
-	//let args = App::new("b8asm")
-	//	.version("0.0")
-	//	.author("Bunogi")
-	//	.args_from_usage("<file> 'assembly file to use'")
-	//	.about("Assembles Chip-8 programs")
-	//	.get_matches(); */
+	let args = App::new("b8asm")
+		.version("0.0")
+		.author("Bunogi")
+		.arg(Arg::with_name("file").help("File to use").required(true))
+		.about("Assembles Chip-8 programs")
+		.get_matches();
 
-	//let file = args.value_of("file").unwrap();
-	match interpret_line(&"ld 0, 0x20".to_string()) {
-		Ok(out) => println!("{:x}", out),
-		Err(e) => println!("Syntax error: {}", e),
+	let file = match File::open(args.value_of("file").unwrap()) {
+		Err(_) => {
+			println!("Failed to open file");
+			process::exit(1);
+		},
+		Ok(file) => file,
+	};
+
+	let file = BufReader::new(file);
+
+	for line in file.lines() {
+		let mut line = line.unwrap();
+		match interpret_line(&mut line) {
+			Ok(out) => println!("{:x}", out),
+			Err(e) => {
+				println!("Syntax error: {}", e);
+				process::exit(2);
+			},
+		}
 	}
-	println!("opcode_convert: {:x}",
-	         convert_to_opcode(CPUInstruction::DRW, 0x0FFF));
 }
