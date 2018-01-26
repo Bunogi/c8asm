fd4abc2e92b53266d3b1b3bd8736caefd902d74b
Bunogi <2bunogi@gmail.com>
2018-01-26 16:17:04 +0100

    Reformat all files

--- src/interpret.rs
+++ src/interpret.rs
@@ -5,161 +5,168 @@ use instructions::*;
 
 //Returns a string with comments removed, with everything in lowercase
 pub fn sanitize_line(input: &mut String) {
-	while let Some(i) = input.find(',') { input.remove(i); }
+    while let Some(i) = input.find(',') {
+        input.remove(i);
+    }
 
-	match input.find(';') {
-		Some(i) => *input = input[0..i].to_string(),
-		None    => {},
-	}
+    match input.find(';') {
+        Some(i) => *input = input[0..i].to_string(),
+        None => {}
+    }
 
-	input.trim_left();
-	input.trim_right();
-	*input = input.to_lowercase();
+    input.trim_left();
+    input.trim_right();
+    *input = input.to_lowercase();
 }
 
 fn convert_number(input: &str) -> Option<u16> {
-	let mut convert = input.to_string();
-	let mut negative = false;
-	if convert.chars().nth(0).unwrap() == '-' {
-		negative = true;
-		convert = convert[1..].to_string();
-	}
+    let mut convert = input.to_string();
+    let mut negative = false;
+    if convert.chars().nth(0).unwrap() == '-' {
+        negative = true;
+        convert = convert[1..].to_string();
+    }
 
-	let result: result::Result<u16, num::ParseIntError>; 
-	if convert.chars().count() > 2 { 
-		//Check if we need to convert from binary or hex 
-		if convert[..2] == *"0x" {
-			result = u16::from_str_radix(&convert[2..], 16);
-		} else if convert[..2] == *"0b" {
-			result = u16::from_str_radix(&convert[2..], 2);
-		} else {
-			result = convert.parse::<u16>();
-		}
-	} else {
-		result = convert.parse::<u16>();
-	}
+    let result: result::Result<u16, num::ParseIntError>;
+    if convert.chars().count() > 2 {
+        //Check if we need to convert from binary or hex
+        if convert[..2] == *"0x" {
+            result = u16::from_str_radix(&convert[2..], 16);
+        } else if convert[..2] == *"0b" {
+            result = u16::from_str_radix(&convert[2..], 2);
+        } else {
+            result = convert.parse::<u16>();
+        }
+    } else {
+        result = convert.parse::<u16>();
+    }
 
-	match result {
-		Ok(num) => {
-			if negative {
-				Some((0x10000 - (num as u32)) as u16)
-			} else {
-				Some(num)
-			}
-		},
-		Err(_)  => None,
-	}
+    match result {
+        Ok(num) => {
+            if negative {
+                Some((0x10000 - (num as u32)) as u16)
+            } else {
+                Some(num)
+            }
+        }
+        Err(_) => None,
+    }
 }
 
 fn interpret_ld_instruction(data: &Vec<&str>) -> Result<(CPUInstruction, u16), String> {
-	//LD Vx, Vy
-	if data[1].chars().nth(0).unwrap() == 'v' {
-		let x = convert_register(data[1])?;
-		if data[2].chars().nth(0).unwrap() == 'v' {
-			let convert = convert_number(&data[2][1..]);
-			match convert {
-				Some(n) => return Ok((CPUInstruction::LD_Vy, (x << 4) | (n << 8))),
-				None    => return Err(format!("Unknown register: {}",  data[2])),
-			}
-		}
-		//LD Vx, *
-		match data[2] {
-			"dt"  => return Ok((CPUInstruction::LD_Vx_DT, convert_register(data[1])? << 8)),
-			"k"   => return Ok((CPUInstruction::LD_K, convert_register(data[1])? << 8)),
-			"[i]" => return Ok((CPUInstruction::LD_Vx_ADDR_I, convert_register(data[1])? << 8)),
-			_     => { //LD Vx, byte
-				let byte: u16;
+    //LD Vx, Vy
+    if data[1].chars().nth(0).unwrap() == 'v' {
+        let x = convert_register(data[1])?;
+        if data[2].chars().nth(0).unwrap() == 'v' {
+            let convert = convert_number(&data[2][1..]);
+            match convert {
+                Some(n) => return Ok((CPUInstruction::LD_Vy, (x << 4) | (n << 8))),
+                None => return Err(format!("Unknown register: {}", data[2])),
+            }
+        }
+        //LD Vx, *
+        match data[2] {
+            "dt" => return Ok((CPUInstruction::LD_Vx_DT, convert_register(data[1])? << 8)),
+            "k" => return Ok((CPUInstruction::LD_K, convert_register(data[1])? << 8)),
+            "[i]" => {
+                return Ok((
+                    CPUInstruction::LD_Vx_ADDR_I,
+                    convert_register(data[1])? << 8,
+                ))
+            }
+            _ => {
+                //LD Vx, byte
+                let byte: u16;
 
-				match convert_number(&data[2]) {
-					Some(n) => byte = n & 0x00FF,
-					None    => return Err(format!("Not a number: {}", data[2])),
-				}
-				return Ok((CPUInstruction::LD, (x << 8) | byte));
+                match convert_number(&data[2]) {
+                    Some(n) => byte = n & 0x00FF,
+                    None => return Err(format!("Not a number: {}", data[2])),
+                }
+                return Ok((CPUInstruction::LD, (x << 8) | byte));
+            }
+        }
+    }
 
-			}
-		}
-	}
-
-	let v = match convert_register(data[2]) {
-		Ok(n)    => n << 8,
-		Err(e)   => return Err(e),
-	};
-	match data[1] {
-		"dt"  => return Ok((CPUInstruction::LD_DT_Vx, v)),
-		"st"  => return Ok((CPUInstruction::LD_ST_Vx, v)),
-		"f"   => return Ok((CPUInstruction::LD_F, v)),
-		"b"   => return Ok((CPUInstruction::LD_B, v)),
-		"[i]" => return Ok((CPUInstruction::LD_ADDR_I_Vx, v)),
-		_     => return Err("Not an ld variant".to_string()),
-	}
+    let v = match convert_register(data[2]) {
+        Ok(n) => n << 8,
+        Err(e) => return Err(e),
+    };
+    match data[1] {
+        "dt" => return Ok((CPUInstruction::LD_DT_Vx, v)),
+        "st" => return Ok((CPUInstruction::LD_ST_Vx, v)),
+        "f" => return Ok((CPUInstruction::LD_F, v)),
+        "b" => return Ok((CPUInstruction::LD_B, v)),
+        "[i]" => return Ok((CPUInstruction::LD_ADDR_I_Vx, v)),
+        _ => return Err("Not an ld variant".to_string()),
+    }
 }
 
 fn convert_register(input: &str) -> Result<u16, String> {
-	if input.chars().nth(0).unwrap() != 'v' {
-		return Err(format!("Invalid register: '{}'", input));
-	}
-	let num = u16::from_str_radix(&input[1..], 16).unwrap();
-	match num > 0xF {
-		true  => Err("Register out of range: ".to_string() + input),
-		false => Ok(num),
-	}
+    if input.chars().nth(0).unwrap() != 'v' {
+        return Err(format!("Invalid register: '{}'", input));
+    }
+    let num = u16::from_str_radix(&input[1..], 16).unwrap();
+    match num > 0xF {
+        true => Err("Register out of range: ".to_string() + input),
+        false => Ok(num),
+    }
 }
 
 pub struct Label {
-	pub name: String,
-	pub offset: usize,
+    pub name: String,
+    pub offset: usize,
 }
 
 //Compare names only
 impl PartialEq for Label {
-	fn eq(&self, other: &Label) -> bool {
-		self.name == other.name
-	}
+    fn eq(&self, other: &Label) -> bool {
+        self.name == other.name
+    }
 }
 
 fn get_zero_count(input: u16) -> usize {
-	let mut n: usize = 0;
-	let input = format!("{:08b}", input).to_string();
+    let mut n: usize = 0;
+    let input = format!("{:08b}", input).to_string();
 
-	for c in input.chars() {
-		if c == '0' {
-			n += 1;
-		} else {
-			break;
-		}
-	}
-	n
+    for c in input.chars() {
+        if c == '0' {
+            n += 1;
+        } else {
+            break;
+        }
+    }
+    n
 }
 
 //Includes preceeding zeroes
 fn get_binary_width(input: &String) -> u8 {
-	let convert = convert_number(input).unwrap();
-	//Function fails for convert == 0, look it up instead
-	if convert == 0 {
-		return 8;
-	}
-	(format!("{:b}", convert).chars().count() + get_zero_count(convert)) as u8
+    let convert = convert_number(input).unwrap();
+    //Function fails for convert == 0, look it up instead
+    if convert == 0 {
+        return 8;
+    }
+    (format!("{:b}", convert).chars().count() + get_zero_count(convert)) as u8
 }
 
 //Returns the instruction and label, as well as whether a label is used
 pub fn interpret_line(data: &Vec<&str>) -> Result<(CPUInstruction, u16, i8), String> {
-	macro_rules! x {
+    macro_rules! x {
 		() => (
 			(convert_register(&data[1])? << 8)
 		)
 	}
-	macro_rules! y {
+    macro_rules! y {
 		() => (
 			(convert_register(&data[2])? << 4)
 		)
 	}
 
-	use instructions::CPUInstruction::*;
+    use instructions::CPUInstruction::*;
 
-	let mut label_pos: i8 = -1;
+    let mut label_pos: i8 = -1;
 
-	//Sets up the return value
-	macro_rules! check_label {
+    //Sets up the return value
+    macro_rules! check_label {
 		($instr:expr, $index:expr) => {
 			match convert_number(data[$index as usize]) {
 				Some(n) => ($instr, n),
@@ -171,82 +178,78 @@ pub fn interpret_line(data: &Vec<&str>) -> Result<(CPUInstruction, u16, i8), Str
 		}
 	}
 
-	let (ins, op) = match data[0] {
-		""    => (blank, 0), //Skip statements without an instruction
-		"cls" => (CLS, 0),
-		"ret" => (RET, 0),
-		"sys" => (SYS, convert_number(data[1]).unwrap()),
-		"jp"  => {
-			match data.len() {
-				2 =>  {
-					check_label!(JP, 1)
-				}
-				3 => {
-					if data[1] != "v0" {
-						return Err("Invalid register: ".to_string() + data[1] + ". Did you mean V0?");
-					} else {
-						check_label!(JP_V0, 2)
-					}
-				},
-				_ => return Err("Too many parameters!".to_string()),
-			}
-		},
-		"call" => check_label!(CALL, 1),
-		"se" => {
-			if data[2].chars().nth(0).unwrap() != 'v' {
-				(SE, x!() | convert_number(data[2]).unwrap())
-			} else {
-				(SE_Vy, x!() | y!())
-			}
-		},
-		"sne" => {
-			if data[2].chars().nth(0).unwrap() != 'v' {
-				(SNE, x!() | convert_number(data[2]).unwrap())
-			} else {
-				(SNE_Vy, x!() | y!())
-			}
-		},
-		"add" => {
-			if data[2].chars().nth(0).unwrap() != 'v' {
-				(ADD, x!() | convert_number(data[2]).unwrap())
-			} else if data[1] == "i" {
-				(ADD_I, y!() << 4)
-			} else {
-				(ADD_Vy, x!() | y!())
-			}
-		},
-		"or"   => (OR,   x!() | y!()),
-		"and"  => (AND,  x!() | y!()),
-		"xor"  => (XOR,  x!() | y!()),
-		"sub"  => (SUB,  x!() | y!()),
-		"shr"  => (SHR,  x!()),
-		"subn" => (SUBN, x!() | y!()),
-		"shl"  => (SHL,  x!()),
-		"rnd"  => (RND,  x!() | (convert_number(data[2]).unwrap() & 0xFF)),
-		"drw"  => (DRW,  x!() | y!() | (convert_number(data[3]).unwrap() & 0xFF)),
-		"skp"  => (SKP,  x!()),
-		"sknp" => (SKNP, x!()),
-		"ld"   =>  {
-			//LD I, addr
-			if data[1] == "i" {
-				check_label!(LD_I, 2)
-			} else {
-				match interpret_ld_instruction(&data) {
-					Ok((ins, op)) => (ins, op),
-					Err(s)        => return Err(s),
-				}
-			}
-		},
-		//Define byte
-		"db" => {
-			let width = get_binary_width(&data[1].to_string());
-			if width != 8 {
-				return Err(format!("Invalid number width: {}. Expected 8", width));
-			}
-			let num = convert_number(data[1]).unwrap();
-			(db, num)
-		}
-		_ => return Err(format!("Unknown instruction: \"{}\"", data[0]).to_string()),
-	};
-	Ok((ins, op, label_pos))
+    let (ins, op) = match data[0] {
+        "" => (blank, 0), //Skip statements without an instruction
+        "cls" => (CLS, 0),
+        "ret" => (RET, 0),
+        "sys" => (SYS, convert_number(data[1]).unwrap()),
+        "jp" => match data.len() {
+            2 => check_label!(JP, 1),
+            3 => {
+                if data[1] != "v0" {
+                    return Err("Invalid register: ".to_string() + data[1] + ". Did you mean V0?");
+                } else {
+                    check_label!(JP_V0, 2)
+                }
+            }
+            _ => return Err("Too many parameters!".to_string()),
+        },
+        "call" => check_label!(CALL, 1),
+        "se" => {
+            if data[2].chars().nth(0).unwrap() != 'v' {
+                (SE, x!() | convert_number(data[2]).unwrap())
+            } else {
+                (SE_Vy, x!() | y!())
+            }
+        }
+        "sne" => {
+            if data[2].chars().nth(0).unwrap() != 'v' {
+                (SNE, x!() | convert_number(data[2]).unwrap())
+            } else {
+                (SNE_Vy, x!() | y!())
+            }
+        }
+        "add" => {
+            if data[2].chars().nth(0).unwrap() != 'v' {
+                (ADD, x!() | convert_number(data[2]).unwrap())
+            } else if data[1] == "i" {
+                (ADD_I, y!() << 4)
+            } else {
+                (ADD_Vy, x!() | y!())
+            }
+        }
+        "or" => (OR, x!() | y!()),
+        "and" => (AND, x!() | y!()),
+        "xor" => (XOR, x!() | y!()),
+        "sub" => (SUB, x!() | y!()),
+        "shr" => (SHR, x!()),
+        "subn" => (SUBN, x!() | y!()),
+        "shl" => (SHL, x!()),
+        "rnd" => (RND, x!() | (convert_number(data[2]).unwrap() & 0xFF)),
+        "drw" => (DRW, x!() | y!() | (convert_number(data[3]).unwrap() & 0xFF)),
+        "skp" => (SKP, x!()),
+        "sknp" => (SKNP, x!()),
+        "ld" => {
+            //LD I, addr
+            if data[1] == "i" {
+                check_label!(LD_I, 2)
+            } else {
+                match interpret_ld_instruction(&data) {
+                    Ok((ins, op)) => (ins, op),
+                    Err(s) => return Err(s),
+                }
+            }
+        }
+        //Define byte
+        "db" => {
+            let width = get_binary_width(&data[1].to_string());
+            if width != 8 {
+                return Err(format!("Invalid number width: {}. Expected 8", width));
+            }
+            let num = convert_number(data[1]).unwrap();
+            (db, num)
+        }
+        _ => return Err(format!("Unknown instruction: \"{}\"", data[0]).to_string()),
+    };
+    Ok((ins, op, label_pos))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 //Command line args
 extern crate clap;
-use clap::{Arg, App};
+use clap::{App, Arg};
 
 use std::fs::File;
 use std::io::{Read, Write};
@@ -13,103 +13,127 @@ mod interpret;
 use interpret::*;
 
 fn main() {
-	let args = App::new("c8asm")
-		.version("0.0.1")
-		.author("Bunogi")
-		.arg(Arg::with_name("file").help("File to assemble").required(true))
-		.arg(Arg::with_name("output").help("Output file").default_value("./a.out").short("o"))
-		.arg(Arg::with_name("stdout").help("Write output to stdout instead of your file")
-			.short("s").conflicts_with("o"))
-		.about("Assembles Chip-8 programs")
-		.get_matches();
+    let args = App::new("c8asm")
+        .version("0.0.1")
+        .author("Bunogi")
+        .arg(
+            Arg::with_name("file")
+                .help("File to assemble")
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("output")
+                .help("Output file")
+                .default_value("./a.out")
+                .short("o"),
+        )
+        .arg(
+            Arg::with_name("stdout")
+                .help("Write output to stdout instead of your file")
+                .short("s")
+                .conflicts_with("o"),
+        )
+        .about("Assembles Chip-8 programs")
+        .get_matches();
 
-	let mut file = match File::open(args.value_of("file").unwrap()) {
-		Err(_) => {
-			println!("Failed to open file");
-			process::exit(1);
-		},
-		Ok(file) => file,
-	};
+    let mut file = match File::open(args.value_of("file").unwrap()) {
+        Err(_) => {
+            println!("Failed to open file");
+            process::exit(1);
+        }
+        Ok(file) => file,
+    };
 
-	let mut lines = String::new();
-	file.read_to_string(&mut lines).unwrap();
+    let mut lines = String::new();
+    file.read_to_string(&mut lines).unwrap();
 
-	let lines: Vec<&str> = lines.split("\n").collect();
-	let mut ready_lines: Vec<String> = Vec::new();
+    let lines: Vec<&str> = lines.split("\n").collect();
+    let mut ready_lines: Vec<String> = Vec::new();
 
-	for line in lines {
-		let mut line = line.to_string();
-		sanitize_line(&mut line);
-		if line != "" {
-			ready_lines.push(line);
-		}
-	}
+    for line in lines {
+        let mut line = line.to_string();
+        sanitize_line(&mut line);
+        if line != "" {
+            ready_lines.push(line);
+        }
+    }
 
-	let mut needs_labels: Vec<(CPUInstruction, &str, usize, i8)> = Vec::new();
-	let mut opcodes:Vec<u8>  = Vec::new();
-	let mut labels:Vec<Label> = Vec::new();
+    let mut needs_labels: Vec<(CPUInstruction, &str, usize, i8)> = Vec::new();
+    let mut opcodes: Vec<u8> = Vec::new();
+    let mut labels: Vec<Label> = Vec::new();
 
-	let mut memory_offset: usize = 0;
+    let mut memory_offset: usize = 0;
 
-	//Splits the opcode. Takes a u16
-	for line in &ready_lines {
-		let data: Vec<&str> = line.split_whitespace().collect();
-		if data.len() == 1 {
-			if let Some(i) = data[0].find(':') {
-				labels.push(Label {name: data[0][0..i].to_string().clone(), 
-					offset: 0x200 + memory_offset});
-				continue;
-			}
-		}
-		use CPUInstruction::db;
-		match interpret_line(&data) {
-			Ok((ins, op, label_pos)) => {
-				if ins == db {
-					memory_offset += 1;
-					opcodes.push(op as u8);
-				} else if label_pos < 0 {
-					let (a, b) = convert_to_opcode(ins, op);
-					opcodes.push(a);
-					opcodes.push(b);
-					memory_offset += 2;
-				} else {
-					opcodes.push(0); opcodes.push(0);
-					needs_labels.push((ins, &line, memory_offset, label_pos));
-					memory_offset += 2;
-				}
-			},
-			Err(e)  => {
-				println!("Syntax error in line \"{}\": {}", &line, e);
-				process::exit(2);
-			},
-		}
-	}
+    //Splits the opcode. Takes a u16
+    for line in &ready_lines {
+        let data: Vec<&str> = line.split_whitespace().collect();
+        if data.len() == 1 {
+            if let Some(i) = data[0].find(':') {
+                labels.push(Label {
+                    name: data[0][0..i].to_string().clone(),
+                    offset: 0x200 + memory_offset,
+                });
+                continue;
+            }
+        }
+        use CPUInstruction::db;
+        match interpret_line(&data) {
+            Ok((ins, op, label_pos)) => {
+                if ins == db {
+                    memory_offset += 1;
+                    opcodes.push(op as u8);
+                } else if label_pos < 0 {
+                    let (a, b) = convert_to_opcode(ins, op);
+                    opcodes.push(a);
+                    opcodes.push(b);
+                    memory_offset += 2;
+                } else {
+                    opcodes.push(0);
+                    opcodes.push(0);
+                    needs_labels.push((ins, &line, memory_offset, label_pos));
+                    memory_offset += 2;
+                }
+            }
+            Err(e) => {
+                println!("Syntax error in line \"{}\": {}", &line, e);
+                process::exit(2);
+            }
+        }
+    }
 
-	for (ins, line, offset, label_pos) in needs_labels {
-		let label = line.split_whitespace().nth(label_pos as usize).unwrap();
-		match labels.iter().find(|&l| *l == Label{name:label.to_string(), offset: 0}) {
-			Some(i) => {
-				let (a, b) = convert_to_opcode(ins, i.offset as u16);
-				opcodes[offset as usize] = a;
-				opcodes[(offset + 1) as usize] = b;
-			}
-			None    => {
-				println!("\"{}\": Unknown label: {}", line, label);
-				process::exit(2);
-			}
-		}
-	}
+    for (ins, line, offset, label_pos) in needs_labels {
+        let label = line.split_whitespace().nth(label_pos as usize).unwrap();
+        match labels.iter().find(|&l| {
+            *l == Label {
+                name: label.to_string(),
+                offset: 0,
+            }
+        }) {
+            Some(i) => {
+                let (a, b) = convert_to_opcode(ins, i.offset as u16);
+                opcodes[offset as usize] = a;
+                opcodes[(offset + 1) as usize] = b;
+            }
+            None => {
+                println!("\"{}\": Unknown label: {}", line, label);
+                process::exit(2);
+            }
+        }
+    }
 
-	if args.is_present("stdout") {
-		for i in opcodes {
-			println!("{:x}", i);
-		}
-	} else {
-		let mut file = File::create(args.value_of("output").expect("No file specified!")).unwrap();
-		match file.write_all(opcodes.as_slice()) {
-			Ok(_) => {},
-			Err(s) => println!("Failed to write to file {}: {}", args.value_of("output").unwrap(), s),
-		}
-
-	}
+    if args.is_present("stdout") {
+        for i in opcodes {
+            println!("{:x}", i);
+        }
+    } else {
+        let mut file = File::create(args.value_of("output").expect("No file specified!")).unwrap();
+        match file.write_all(opcodes.as_slice()) {
+            Ok(_) => {}
+            Err(s) => println!(
+                "Failed to write to file {}: {}",
+                args.value_of("output").unwrap(),
+                s
+            ),
+        }
+    }
 }
